bdf03d552a2619b96553b5fb0f55f6ce25c6002a
Simon Raming <simon.raming@rwth-aachen.de>
2023-12-04 23:44:10 +0100

    fileUtil extend supported keywords

--- pyStadt/core/obejcts/fileUtil.py
+++ pyStadt/core/obejcts/fileUtil.py
@@ -7,7 +7,9 @@ class CityFile:
         cityGMLversion: str,
         building_ids: list,
         ades: list,
+        srsName: str = None,
         gmlName: str = None,
+        identifier: str = None,
     ) -> None:
         """stores information from an imported file
 
@@ -21,15 +23,22 @@ class CityFile:
             list of all building ids in given file
         ades : list
             list of recognized ADEs
+        srsName : str, optional
+            srsName of the file, by default None
         gmlName : str, optional
             content of gml:name element which is child of core:CityModel,
+            by default None
+        identifier : str, optional
+            identifier of the file, e.g. identifier from metadata of CityJSON file,
             by default None
         """
         self.filepath = filepath
         self.cityGMLversion = cityGMLversion
         self.building_ids = building_ids
         self.ades = ades
+        self.srsName = srsName
         self.gmlName = gmlName
+        self.identifier = identifier
 
         self.lowerCorner = None
         self.upperCorner = None
