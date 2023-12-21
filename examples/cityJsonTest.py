58865f1078d2877db62861907371d94b71491f12
Simon Raming <simon.raming@rwth-aachen.de>
2023-12-22 00:27:33 +0100

    cityjson example change path

--- examples/cityJsonTest.py
+++ examples/cityJsonTest.py
@@ -1,11 +1,9 @@
 from pyStadt import Dataset
 from pyStadt.core.input.cityjsonInput import load_buildings_from_json_file
 
-x = "/Users/simon/Downloads/twobuildings.city.json"
 newDataset = Dataset()
-load_buildings_from_json_file(newDataset, x)
-
-print(newDataset.size())
+load_buildings_from_json_file(newDataset, "examples/files/twobuildings.city.json")
+    
 
 from pyStadt.core.output.cityjsonOutput import write_ciyjson_file
 write_ciyjson_file(newDataset, "test.json")
