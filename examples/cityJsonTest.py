99a4e8d2bf92e3a575042b71883dccc60f11d867
Simon Raming <simon.raming@rwth-aachen.de>
2024-03-13 17:48:11 +0100

    delete old cityjson example

--- examples/cityJsonTest.py
+++
@@ -1,9 +0,0 @@
-from pyStadt import Dataset
-from pyStadt.core.input.cityjsonInput import load_buildings_from_json_file
-
-newDataset = Dataset()
-load_buildings_from_json_file(newDataset, "examples/files/twobuildings.city.json")
-    
-
-from pyStadt.core.output.cityjsonOutput import write_cityjson_file
-write_cityjson_file(newDataset, "test.json")
