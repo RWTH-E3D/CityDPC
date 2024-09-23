5c486949df39ed8a7083e40a8e37f4990262bb47
Simon Raming <simon.raming@rwth-aachen.de>
2024-09-23 19:27:06 +0200

    adjust osm example

--- examples/osmExample.py
+++ examples/osmExample.py
@@ -10,18 +10,33 @@ with open("examples/exampleQuery.json") as f:
 
 # create empty dataset
 dataset = Dataset()
-t = Transformer.from_crs("epsg:4326", "epsg:2555")
+t = Transformer.from_crs("epsg:4326", "epsg:5555")
 
-# load osm data
-for b in osmQuery["elements"]:
-    # load coordinates of building outline
+# split query into dict of nodes and ways for easier access
+nodes = {}
+ways = {}
+for element in osmQuery["elements"]:
+    if element["type"] == "node":
+        nodes[element["id"]] = element
+    elif element["type"] == "way":
+        ways[element["id"]] = element
+
+# transform all nodes to local coordinate system
+for node in nodes.values():
+    lat, lon = t.transform(node["lat"], node["lon"])
+    node["lat"] = round(lat, 3)
+    node["lon"] = round(lon, 3)
+
+# iterate over ways and create building objects
+for bID, obj in ways.items():
     coordinates = []
-    for point in b["geometry"]:
-        # transform coordinates to local coordinate system
-        coordinates.append(t.transform(point["lat"], point["lon"]))
-    # create a building with a flat roof
-    building = cityBIT.create_LoD2_building(b["id"], coordinates, 160, 10, "1000")
-    dataset.buildings[b["id"]] = building
+    # iterate over nodes and append them to the coordinates list
+    for node in obj["nodes"]:
+        coordinates.append([nodes[node]["lat"], nodes[node]["lon"]])
+    # create building object with a ground surface height of 160m and
+    # building height of 12m with a flat roof (code: "1000")
+    building = cityBIT.create_LoD2_building(str(bID), coordinates, 160, 12, "1000")
+    # add building object to dataset
+    dataset.buildings[building.gml_id] = building
 
-# save dataset as cityjson
-cityjsonOutput.write_cityjson_file(dataset, "osmExample.json")
+cityjsonOutput.write_cityjson_file(dataset, "example.city.json")
